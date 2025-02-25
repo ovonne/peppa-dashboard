7f0afde95cf8e471ed1b427b331f99bd9199c5da
Adao-Angelo <adaobegginer@gmail.com>
2025-02-25 13:59:04 +0100

    update zustand error

--- src/store/inCharge/index.tsx
+++ src/store/inCharge/index.tsx
@@ -1,26 +1,26 @@
-import { inChargeService } from "@/services/InChargeService";
-import type { InChargeType } from "@/types";
-import { create } from "zustand";
+// import { inChargeService } from "@/services/InChargeService";
+// import type { InChargeType } from "@/types";
+// import { create } from "zustand";
 
-interface InChargeStore {
-  inCharge: string | null;
-  updateInCharge: (inChargeId: string) => Promise<void>;
-  removeInCharge: () => Promise<void>;
-}
+// interface InChargeStore {
+//   inCharge: string | null;
+//   updateInCharge: (inChargeId: string) => Promise<void>;
+//   removeInCharge: () => Promise<void>;
+// }
 
-const useInChargeStore = create<InChargeStore>((set) => ({
-  inCharge: null,
+// const useInChargeStore = create<InChargeStore>((set) => ({
+//   inCharge: null,
 
-  updateInCharge: async (
-    id: string,
-    data: Partial<Omit<InChargeType, "id">>,
-  ) => {
-    await inChargeService.update(id, data);
-  },
+//   updateInCharge: async (
+//     id: string,
+//     data: Partial<Omit<InChargeType, "id">>,
+//   ) => {
+//     await inChargeService.update(id, data);
+//   },
 
-  removeInCharge: async (id: string) => {
-    await inChargeService.delete(id);
-  },
-}));
+//   removeInCharge: async (id: string) => {
+//     await inChargeService.delete(id);
+//   },
+// }));
 
-export default useInChargeStore;
+// export default useInChargeStore;
